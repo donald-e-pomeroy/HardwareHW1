7b04dfb6bdac13fea8e1603457e4c1b4c14559e1
lcr2131 <robinson.leonardc@gmail.com>
2012-09-26 11:18:01 -0400

    Update the cam file.

--- cam.sv
+++ cam.sv
@@ -1,3 +1,15 @@
-module cam()
+module cam(read_i, 
+	   read_index_i, 
+	   write_i, 
+	   write_index_i, 
+	   write_data_i, 
+	   search_i, 
+	   search_data_i, 
+	   read_valid_o,
+	   read_value_o,
+	   search_valid_o
+	   search_index_o);
+
+   
 
   endmodule
