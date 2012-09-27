942625ebdfaecb57ad2b8a15266ff42b6409ecbd
lcr2131 <robinson.leonardc@gmail.com>
2012-09-27 11:34:23 -0400

    Updated Versions

--- cam.sv
+++ cam.sv
@@ -1,15 +1,43 @@
-module cam(read_i, 
-	   read_index_i, 
-	   write_i, 
-	   write_index_i, 
-	   write_data_i, 
-	   search_i, 
-	   search_data_i, 
-	   read_valid_o,
-	   read_value_o,
-	   search_valid_o
-	   search_index_o);
+module cam#(parameter DATA_WIDTH = 32,
+	    parameter ADDR_WIDTH = 5,
+	    parameter DEPTH = (1 << ADDR_WIDTH),
+	    parameter SIZE = (DEPTH*DATA_WIDTH))
+   //End Parameter Defs
+  (
+   input clk, read_i, write_i, search_i;
+   input[4:0] read_index_i, write_index_i;
+   input[31:0] write_data_i, search_data_i;
+   
+   output logic     read_valid_o, search_valid_o ;
+   output logic [4:0] search_index_o;
+   output logic [31:0] read_value_o;
+   //Possible Struct Defs?
+   ); // End Input Defs
+   
+   //MUX
+   wire [SIZE - 1 : 0 ] all_data;
+   //Insert MUX Here
+   
+   //Decoder
+   //Put inputs on wires inside of the CAM
+   wire [ADDR_WIDTH - 1 : 0] read_index;
+   wire [DEPTH - 1 : 0] write_data_i;
+   wire [DEPTH - 1 : 0] search_data_i;
 
+   cam_decoder decoder(.write_i, .search_i, );
+   
+
+
+   //Priority Encoder
+
+
+
+
+   //Generator
+      
+
+   end
+   
    
 
   endmodule
--- cam_decoder.sv
+++ cam_decoder.sv
@@ -1,5 +1,25 @@
-module cam_decorder#()();
+//Leonard Robinson
+
+module cam_decoder#(parameter DATA_WIDTH = 32,
+		    parameter ADDR_WIDTH = 5,
+		    parameter DEPTH = (1<<ADDR_WIDTH),
+		    parameter SIZE = (DEPTH*DATA_WIDTH))
+   //End Parameter Defs
+   (input write_i,
+    input search_i,
+    input[ADDR_WIDTH - 1 : 0 ] address_i,
+    output logic [DEPTH - 1 : 0]write_enable_o,
+    output logic [DEPTH - 1 : 0]read_enable_o,
+    output logic [DEPTH - 1 : 0]search_enable_o
+    );
+
+   logic [DEPTH - 1 : 0] 	enable;
+   always_comb begin
+
+   end
+   
 
 
 
-endmodule // cam_decorder
+
+endmodule // cam_decoder
