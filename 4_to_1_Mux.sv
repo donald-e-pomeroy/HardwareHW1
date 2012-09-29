97d76365d5779b0c1b22dde3405452565f633d65
TheDonald <thedonald@ubuntu.(none)>
2012-09-28 20:07:15 -0400

    4_to_1_Mux compiles

--- 4_to_1_Mux.sv
+++ 4_to_1_Mux.sv
@@ -1,20 +1,20 @@
-module 4_to_1_mux(
-	input input_lines[3:0],
-	input selector_bits[1:0],
+module fourToOneMux(
+	input [3:0] input_lines,
+	input [1:0] selector_bits,
 	output logic output_line);
 	
-
+	
 	always_comb begin
-		assign output_line = ((~selector_bit[1])&
-		(~selector_bit[0])&input_line[0])
+		assign output_line = ((~selector_bits[1])&
+		(~selector_bits[0])&input_lines[0])
 		|
-		((~selector_bit[1])&
-		(selector_bit[0])&input_line[1])
+		((~selector_bits[1])&
+		(selector_bits[0])&input_lines[1])
 		|
-         	((selector_bit[1])&
-		(~selector_bit[0])&input_line[2])
+         	((selector_bits[1])&
+		(~selector_bits[0])&input_lines[2])
 		|
-		((selector_bit[1])&
-		(selector_bit[0])&input_line[3]);
+		((selector_bits[1])&
+		(selector_bits[0])&input_lines[3]);
 	end
 endmodule
