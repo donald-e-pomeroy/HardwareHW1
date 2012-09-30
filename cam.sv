49f55510bde14618d2a1228779d206de4b2bc070
lcr2131 <robinson.leonardc@gmail.com>
2012-09-30 13:26:59 -0400

    Doin it again.

--- cam.sv
+++ cam.sv
@@ -2,7 +2,7 @@
 // 
 // Date Created:
 // Date Modified:
-module cam #(parameter WIDTH = 32,parameter ADDR_WIDTH = 5)
+module cam #(parameter WIDTH = 32,parameter ADDR_WIDTH = 5, parameter HEIGHT = 32)
    //End Parameter Defs
   (
    input 	       clk_i, 
@@ -30,28 +30,23 @@ module cam #(parameter WIDTH = 32,parameter ADDR_WIDTH = 5)
    wire [width -1 : 0] dec_write_enable;
    wire [width -1 : 0] dec_compare_enable;
 
-   cam_dec_rev decoder(.read_enable_i, 
-.read_index_i, 
-.write_enable_i,
-.write_index_i,
-.write_data_i,
-.search_enable_i,
-.search_data_i,
-.data_o(dec_data_o),
-.data_addr_o(dec_addr_o));
+   wire [ADDR_WIDTH - 1 :0 ] selector_bits;
+   wire [WIDTH -1 :0] input_line;
+
+   cam_dec_rev decoder(.*,.data_o(dec_data_o),.data_addr_o(dec_addr_o));
    
   //Memory Generation
   //Makin 32 of these.	
 
 row row1(.clk, .rst_i, ., .write_enable_i, .search_enable_i, .search_data_i);
    
-row row2()
-
-row row3()
-
-row row4()
-//   priority_encoder4 priority_en(,,);
+   priority_encoder priority_en(,,.data_o(selector_bits));
    
-//   32_to_1_Mux mux(,,);
-   
+   	
+
+//   32_to_1_Mux mux(,,);emac 
+	for(iter = 0; iter < HEIGHT; iter++)begin
+		ThirtyTwoToOneMux mux(.input_line(),.selector_bits,.output_line());//TODO		
+	end   
+
 endmodule // cam
