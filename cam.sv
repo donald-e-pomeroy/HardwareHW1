af1e67caa7fc8b7c163e790abc6910e89bf9852d
Corey C Stappenbeck <ccs2135@suva.clic.cs.columbia.edu>
2012-10-01 23:20:55 -0400

    Update

--- cam.sv
+++ cam.sv
@@ -15,7 +15,7 @@ module cam #(parameter WIDTH = 32,parameter ADDR_WIDTH = 5)
    input 			search_enable_i, 
    input [WIDTH - 1 : 0 ] 	search_data_i,
    
-   output 			read_valid_o,
+   output logic			read_valid_o,
    output [WIDTH - 1 : 0] 	read_value_o,
    output 			search_valid_o,
    output [ADDR_WIDTH - 1 : 0 ] search_index_o
@@ -28,7 +28,9 @@ module cam #(parameter WIDTH = 32,parameter ADDR_WIDTH = 5)
    wire [WIDTH - 1 : 0] 	dec_write_enable;
    wire [WIDTH - 1 : 0] 	dec_search_enable;
    
-   wire [WIDTH - 1 : 0]		row_out_into_mux;
+   typedef logic [31:0] 	ThirtyTwoInput;
+   ThirtyTwoInput [WIDTH - 1 : 0] row_out_into_mux;
+   
 
    wire [ADDR_WIDTH - 1 : 0 ] 	selector_bits;
    wire [WIDTH - 1 : 0] 	input_line;
@@ -46,15 +48,19 @@ module cam #(parameter WIDTH = 32,parameter ADDR_WIDTH = 5)
          row		 membit(.clk(clk_i),
 				.reset(rst_i),
 				.data_i(write_data_i),//Connect All Input Bits to All Row bits
-				.write_enable_i,
+				.write_enable_i(dec_write_enable[j]),
 				.search_enable_i,
 				.search_data_i,//Connect All the Data Ports!
-				.data_o(row_out_into_mux),//output read line
+				.data_o(row_out_into_mux[j]),//output read line
 				.match_o(match[j]),
-				.read_valid_o(read_valid_o)
+				.read_valid_o(readlogic[j])
 				);
       end                
    endgenerate
+
+   always_comb begin
+      read_valid_o = readlogic[read_index_i];
+   end
 
    ThirtyTwoByThirtyTwoMux mux(.data_i(row_out_into_mux),.selector_bits(read_index_i),.data_o(read_value_o) );
    
